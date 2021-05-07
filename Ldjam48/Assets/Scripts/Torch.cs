50dc632fcac287afe97ecb21da2e237456f40e5b
Benjamin Prats <prats.benjamin@gmail.com>
2021-05-06 23:10:51 -0400

    Basic torch system

--- Ldjam48/Assets/Scripts/Torch.cs
+++ Ldjam48/Assets/Scripts/Torch.cs
@@ -6,8 +6,25 @@ public class Torch : MonoBehaviour
 {
 	[SerializeField] Light _light;
 
-	private void Update()
+	private void OnTriggerStay(Collider other)
 	{
-		
+		Debug.Log("Detected");
+		PlayerController player = other.gameObject.GetComponent<PlayerController>();
+		if (player)
+		{
+			if (Input.GetKeyDown(KeyCode.E))
+			{
+				Light();
+				// Call player animation
+			}
+		}
 	}
+
+	private void Light()
+	{
+		// Do the actual lighting and deactivate the script
+		_light.gameObject.SetActive(true);
+		this.enabled = false;
+	}
+
 }
